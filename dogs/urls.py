51663e5304533dfa939092529009f38003ec9a12
anor23 <burgchiken@gmail.com>
2025-03-25 18:45:50 +0300

    создана dogform в файле forms новая функция представления dogs_сreate настроены урлы для новой функции

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,7 +1,7 @@
 from  django.urls import path
 
 
-from dogs.views import index, breeds_list, breeds_list_view, dogs_list_view
+from dogs.views import index, breeds_dogs_list_view, breeds_list_view, dogs_list_view, dog_create_view
 from  dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -10,7 +10,8 @@ app_name = DogsConfig.name
 urlpatterns = [
     path('',index,name='index'),
     path('breeds/',breeds_list_view,name='breeds'),
-    path('breeds/<int:pk>/dogs/',breeds_list_view,name = 'breed_dogs'),
+    path('breeds/<int:pk>/dogs/',breeds_dogs_list_view,name = 'breed_dogs'),
 
-    path('dogs/',dogs_list_view,name="dogs_list")
+    path('dogs/',dogs_list_view,name="dogs_list"),
+    path('dogs/create/',dog_create_view,name='dog_create'),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from dogs.models import Breed, Dog
-
+from forms import DogForm
 
 # Create your views here
 def index(request):
@@ -11,7 +12,8 @@ def index(request):
     }
     return render(request, "dogs/index.html", context)
 
-def breeds_list(request):
+
+def breeds_list_view(request):
     context = {
         'objects_list': Breed.objects.all(),
         'title': 'Питомник - Все наши породы'
@@ -19,14 +21,14 @@ def breeds_list(request):
     return render(request, "dogs/breeds.html", context)
 
 
-def breeds_list_view(request, pk: int):
+def breeds_dogs_list_view(request, pk: int):
     breed_item = Breed.objects.get(pk=pk)
     context = {
         'objects_list': Dog.objects.filter(breed_id=pk),
         'title': f'Cобаки породы - {breed_item.name}',
         'breed_pk': breed_item.pk,
     }
-    return render(request,'dogs/dogs.html',context)
+    return render(request, 'dogs/dogs.html', context)
 
 
 def dogs_list_view(request):
@@ -34,4 +36,13 @@ def dogs_list_view(request):
         'objects_list': Dog.objects.all(),
         'title': 'Питомник - Все наши собаки'
     }
-    return render(request,'dogs/dogs.html',context)
+    return render(request, 'dogs/dogs.html', context)
+
+
+def dog_create_view(request):
+    if request.method == "POST":
+        form = DogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('dogs:dogs_list'))
+    return render(request,'dogs/create.html',{'form':DogForm()})
