963b04f037d4da5b2ca1177529f33e86fe2b3fe5
anor23 <burgchiken@gmail.com>
2025-03-25 18:23:52 +0300

    модель  dogs зарегстрирована в админке

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from dogs.models import Breed ,Dog
+
+
 # Register your models here.
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ('pk','name',)
+    ordering = ('pk',)
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name','breed',)
+    ordering =  ('name',)
