3ce593b3ce5306c7e03b1f65ba0e6e2252898461
anor23 <burgchiken@gmail.com>
2025-03-25 18:23:31 +0300

    настроены urls для новых шаблонов

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,7 +1,7 @@
 from  django.urls import path
 
 
-from dogs.views import index
+from dogs.views import index, breeds_list, breeds_list_view, dogs_list_view
 from  dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -9,4 +9,8 @@ app_name = DogsConfig.name
 
 urlpatterns = [
     path('',index,name='index'),
+    path('breeds/',breeds_list_view,name='breeds'),
+    path('breeds/<int:pk>/dogs/',breeds_list_view,name = 'breed_dogs'),
+
+    path('dogs/',dogs_list_view,name="dogs_list")
 ]
