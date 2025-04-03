8218c75df2308eb6078f940608f57b3db1700efc
anor23 <burgchiken@gmail.com>
2025-04-03 12:16:15 +0300

    пути для новых функций написаны

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from users.apps import UsersConfig
 from users.views import user_register_view, user_login_view, user_profile_view, user_logged_out_view, user_update_view, \
-    user_change_password_view
+    user_change_password_view, user_generate_new_passport_view
 
 app_name = UsersConfig.name
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path("profile/", user_profile_view, name="user_profile"),
     path('logout', user_logged_out_view, name='user_logout', ),
     path('update/', user_update_view, name= 'user_update',),
-    path("change_password/",user_change_password_view,name='user_change_password')
+    path("change_password/",user_change_password_view,name='user_change_password'),
+    path('profile/genpassword/',user_generate_new_passport_view,name='user_generate_new_password')
 ]
