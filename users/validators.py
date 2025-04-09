90d033e9b6c6d9d9587b703ed0979bd614448edd
anor23 <burgchiken@gmail.com>
2025-04-09 18:03:10 +0300

    улучшена функция валидации пароля

--- users/validators.py
+++ users/validators.py
@@ -3,12 +3,28 @@ from django.core.exceptions import ValidationError
 
 def validate_password(filed):
     pattern = re.compile(r'^[A-Za-z0-9]+$')
-    if not bool(re.match(pattern,filed)):
-        ex = 'Пароль должен только содержать латинские буквы и цифры'
-        print(ex)
-        raise ValidationError (ex)
-    if not 8<= len(filed) <=16:
-        ex = "Пароль должен быть от 8 до 16 символов"
-        print(ex)
-        raise ValidationError(ex)
+    error_messages  =[
+        {
+            'ru-ru':'Пароль должен содержать только символы латинского алфавита и цифры!',
+            'en-us':'The password must contain only Latin characters and numbers!'
 
+        },
+        {
+            'ru-ru':'Длина пароля должна быть между 8 и 16  символами!',
+            'en-us':'The password must be between 8 and 16 characters long!',
+        }
+    ]
+    if not bool(re.match(pattern,filed)):
+        print(error_messages[0][language])
+
+
+        raise ValidationError (
+            error_messages[0][language],
+            code = error_messages[0][language]
+        )
+    if not 8<= len(filed) <=16:
+        print(error_messages[1][language])
+        raise ValidationError(
+            error_messages[1][language],
+            code=error_messages[1][language]
+        )
