974fea0412a3f9408716c848efe56cacc9e84d12
anor23 <burgchiken@gmail.com>
2025-04-16 18:26:17 +0300

    обработка вывода кнопок добавлена для админа вывод

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
             email = "admin@web.top",
             first_name = "Andrei",
             last_name = "Nikanov",
+            role= 'admin',
             is_staff = True,
             is_superuser = True,
             is_active = True,
@@ -19,3 +20,32 @@ class Command(BaseCommand):
         admin_user.set_password("querty")
         admin_user.save()
         print("admin_created")
+
+        moderator = User.objects.create(
+            email = "moder@web.top",
+            first_name = "Moder",
+            last_name = "Moderatorov",
+            role= 'moderator',
+            is_staff = True,
+            is_superuser = False,
+            is_active = True,
+            date_birth = "2000-04-30"
+        )
+
+        moderator.set_password('qwerty')
+        moderator.save()
+        print('Moderator Created')
+
+        user = User.objects.create(
+            email="user@web.top",
+            first_name="user",
+            last_name="userov",
+            role='moderator',
+            is_staff=False,
+            is_superuser=False,
+            is_active=True,
+            date_birth="2000-04-30"
+        )
+        user.set_password('qwerty')
+        user.save()
+        print('user Created')
