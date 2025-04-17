fac60a9c32a3815c571e226eb97dd2fbcb41de6b
anor23 <burgchiken@gmail.com>
2025-04-17 13:49:46 +0300

    добавлено новое поле просмотров

--- dogs/models.py
+++ dogs/models.py
@@ -24,6 +24,7 @@ class Dog(models.Model):
     is_active = models.BooleanField(default=True, verbose_name="Активно")
 
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Хозяин")
+    views = models.IntegerField(default=0 , verbose_name='Просмотры')
 
     def __str__(self):
         return f'{self.name} ({self.breed})'
@@ -33,6 +34,9 @@ class Dog(models.Model):
         verbose_name_plural = "dogs"
         db_table = 'our_dogs'
 
+    def views_count(self):
+        self.views += 1
+        self.save()
 
 class DogParent(models.Model):
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
