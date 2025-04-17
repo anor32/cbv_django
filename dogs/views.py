fd8b00d83296218bd52fec9d1e109ed3ac3615b6
anor23 <burgchiken@gmail.com>
2025-04-17 13:49:20 +0300

    добавлено разграничение по правам

--- dogs/views.py
+++ dogs/views.py
@@ -1,17 +1,19 @@
 from lib2to3.fixes.fix_input import context
 
 from PIL.ImageFilter import DETAIL
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin , PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, Http404
+from django.http import  Http404, HttpResponseForbidden
 
 from django.forms import inlineformset_factory
 
 from django.urls import reverse, reverse_lazy
 from dogs.models import Breed, Dog, DogParent
-from dogs.forms import DogForm, DogParentForm
+from dogs.forms import DogForm, DogParentForm, DogAdminForm
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
+from dogs.servises import send_views_mail
 from users.models import UserRoles
 
 
@@ -41,21 +43,12 @@ class BreedListView(LoginRequiredMixin,ListView):
         'title':"Все наши породы"
     }
     template_name = 'dogs/breeds.html'
-# def breeds_dogs_list_view(request, pk: int):
-#     breed_item = Breed.objects.get(pk=pk)
-#     context = {
-#         'object_list': Dog.objects.filter(breed_id=pk),
-#         'title': f'Cобаки породы - {breed_item.name}',
-#         'breed_pk': breed_item.pk,
-#     }
-#     return render(request, 'dogs/dogs.html', context)
 
 
 class DogListView(ListView):
     model = Dog
     extra_context = {
         'title': 'Питомник - Все наши породы'
-
     }
     template_name = 'dogs/dogs.html'
     def get_queryset(self):
@@ -88,6 +81,8 @@ class DogCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('dogs:dogs_list')
 
     def form_valid(self, form):
+        if self.request.user.role != UserRoles.USER:
+            raise  PermissionDenied
         self.object = form.save()
         self.object.owner = self.request.user
         return super().form_valid(form)
@@ -103,6 +98,14 @@ class DogDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         dog_object = self.get_object()
         context['title'] = f'вы выбрали {dog_object.name}, Порода {dog_object.breed.name}'
+        dog_object_increase = get_object_or_404(Dog, pk=dog_object.pk)
+        if dog_object.owner != self.request.user:
+            dog_object_increase.views_count()
+        if dog_object.owner:
+            dog_object_owner_email = dog_object.owner.email
+            if dog_object_increase.views%20 == 0 and dog_object_increase != 0:
+                 send_views_mail(dog_object_increase.name,dog_object_owner_email, dog_object_increase.views)
+
         return context
 
 
@@ -120,9 +123,20 @@ class DogUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
-        if self.object.owner != self.request.user and not self.request.user.is_staff:
-            raise Http404
+        # if self.object.owner != self.request.user and not self.request.user.is_staff:
+        if self.object.owner != self.request.user and self.request.user.role != UserRoles.ADMIN:
+            raise PermissionDenied()
         return self.object
+
+    def get_form_class(self):
+        dog_forms = {
+            'admin':DogAdminForm,
+            'moderator':DogForm,
+            'user':DogForm,
+        }
+        user_role =self.request.user_role
+        dog_form_class = dog_forms[user_role]
+        return dog_form_class
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -145,13 +159,15 @@ class DogUpdateView(LoginRequiredMixin, UpdateView):
 
         return super().form_valid(form)
 
-class DogDeleteView(LoginRequiredMixin, DeleteView):
+class DogDeleteView(PermissionRequiredMixin, DeleteView):
     model = Dog
-    template_name = 'dogs/'
+    template_name = 'dogs/delete.html'
     extra_context = {
         'title': "удалить собаку"
     }
     success_url = reverse_lazy("dogs:dogs_list")
+    permission_required = 'dogs.delete_dog'
+    permission_denied_message = "Не достаточно прав"
 
 def toggle_activity(request, pk):
     dog_item = get_object_or_404(Dog,pk=pk)
