74ac8eccd3ea0b1fd23100a9f45f4118b6302d78
anor23 <burgchiken@gmail.com>
2025-04-17 13:47:35 +0300

    повышена надежность формы добавлена форма для админа

--- dogs/forms.py
+++ dogs/forms.py
@@ -6,21 +6,32 @@ from dogs.models import Dog, DogParent
 from users.forms import StyleFromMixin
 
 
-class DogForm(StyleFromMixin,forms.ModelForm):
+class DogForm(StyleFromMixin, forms.ModelForm):
     class Meta:
         model = Dog
-        exclude = ('owner',"is_active")
+        exclude = ('owner', "is_active", "views")
 
     def clean_birth_date(self):
         cleaned_data = self.cleaned_data['birth_date']
-        now_year = datetime.datetime.now().year
-        if now_year - cleaned_data.year >35:
-            raise  forms.ValidationError('Cобака должна быть моложе 35 лет')
+        if cleaned_data:
+            now_year = datetime.datetime.now().year
+            if now_year - cleaned_data.year > 35:
+                raise forms.ValidationError('Cобака должна быть моложе 35 лет')
         return cleaned_data
 
 
+class DogAdminForm(DogForm):
+    class Meta:
+        model = Dog
+        exclude = ("is_active",)
 
-class DogParentForm(StyleFromMixin,forms.ModelForm):
+
+    def clean_birth_date(self):
+        cleaned_data = super().clean_birth_date()
+        return cleaned_data
+
+
+class DogParentForm(StyleFromMixin, forms.ModelForm):
     class Meta:
         model = DogParent
         fields = '__all__'
